2f8c44d6615cab8db67ead8ce84b55da86c753cc
Ivan Atias <ivan.d.atias@gmail.com>
2023-11-22 15:20:36 -0400

    🔧 refactor: extract feed skeletons in constants

--- src/app/(with-main)/page.tsx
+++ src/app/(with-main)/page.tsx
@@ -1,41 +1,14 @@
 import { Suspense } from 'react'
 import { FeedServer } from '@/components/feed/feed-server'
 import { PostCardSkeleton } from '@/components/post/components/post-skeleton'
-
-const SKELETONS_DIMENSIONS = [
-  {
-    width: 320,
-    height: 570
-  },
-  {
-    width: 320,
-    height: 300
-  },
-  {
-    width: 320,
-    height: 500
-  },
-  {
-    width: 300,
-    height: 500
-  },
-  {
-    width: 370,
-    height: 565
-  },
-
-  {
-    width: 240,
-    height: 390
-  }
-]
+import { FEED_SKELETONS_DIMENSIONS } from '@/constants/skeletons'
 
 export default function Home() {
   return (
     <Suspense
       fallback={
         <div className='flex flex-wrap gap-6 w-full'>
-          {SKELETONS_DIMENSIONS.map((dimension, index) => (
+          {FEED_SKELETONS_DIMENSIONS.map((dimension, index) => (
             <PostCardSkeleton
               key={index}
               width={dimension.width}
