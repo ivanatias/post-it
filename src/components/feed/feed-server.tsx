abe3b3a404c822072770aa542e0e46ac3de9ce82
Ivan Atias <ivan.d.atias@gmail.com>
2023-11-29 00:21:54 -0400

    🔧 refactor: make feed component get many kind of feeds

--- src/components/feed/feed-server.tsx
+++ src/components/feed/feed-server.tsx
@@ -2,13 +2,52 @@ import { currentUser } from '@clerk/nextjs'
 import { PostCard } from '../post'
 import { MasonryLayout } from '../utils/masonry-layout'
 import { client } from '@/lib/sanity/client'
-import { getAllPostsQuery } from '@/lib/sanity/queries'
+import {
+  getAllPostsQuery,
+  getPostsByCategoryQuery,
+  getPostsByUserQuery
+} from '@/lib/sanity/queries'
 import { parseUserID } from '@/lib/utils'
 import type { Post } from '@/lib/sanity/types/post'
 
-export async function FeedServer() {
+type FeedProps =
+  | {
+      type: 'main'
+    }
+  | {
+      type: 'category'
+      category: string
+    }
+  | {
+      type: 'user'
+      userID: string
+    }
+  | {
+      type: 'search'
+      term: string
+    }
+
+export async function FeedServer(props: FeedProps) {
+  let queryForFeed = ''
+
+  if (props.type === 'category') {
+    queryForFeed = getPostsByCategoryQuery(props.category)
+  }
+
+  if (props.type === 'user') {
+    queryForFeed = getPostsByUserQuery(props.userID)
+  }
+
+  if (props.type === 'search') {
+    queryForFeed = 'todo'
+  }
+
+  if (props.type === 'main') {
+    queryForFeed = getAllPostsQuery()
+  }
+
   const [posts, user] = await Promise.all([
-    client.fetch<Post[]>(getAllPostsQuery()),
+    client.fetch<Post[]>(queryForFeed, {}, { cache: 'no-store' }),
     currentUser()
   ])
 
