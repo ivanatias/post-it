a7e9c1c66c50fa5f427bdb1d781358f387cfae16
Ivan Atias <ivan.d.atias@gmail.com>
2023-11-29 00:31:35 -0400

    🐛 fix: loading suspense boundary showing on navigation
    suspense boundary from top-most loading.tsx file was showing when navigating to a post page

--- src/app/(with-main)/page.tsx
+++ src/app/(with-main)/page.tsx
@@ -1,5 +1,24 @@
+import { Suspense } from 'react'
 import { FeedServer } from '@/components/feed/feed-server'
+import { PostCardSkeleton } from '@/components/post/components/post-skeleton'
+import { FEED_SKELETONS_DIMENSIONS } from '@/constants/skeletons'
 
 export default async function Home() {
-  return <FeedServer type='main' />
+  return (
+    <Suspense
+      fallback={
+        <div className='flex flex-wrap gap-6 w-full'>
+          {FEED_SKELETONS_DIMENSIONS.map((dimension, index) => (
+            <PostCardSkeleton
+              key={index}
+              width={dimension.width}
+              height={dimension.height}
+            />
+          ))}
+        </div>
+      }
+    >
+      <FeedServer type='main' />
+    </Suspense>
+  )
 }
