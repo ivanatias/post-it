e857b6a453d91a60f823c20d50d03cc63acab93c
Ivan Atias <ivan.d.atias@gmail.com>
2023-11-29 20:14:38 -0400

    🔧 refactor: make logo link to homepage

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,9 @@ import { PlusIcon, SearchIcon } from 'lucide-react'
 export function Navbar() {
   return (
     <nav className='relative flex items-center justify-between w-full gap-3 py-3'>
-      <img src='/logo.svg' alt='Post it logo' width={140} height={80} />
+      <Link href='/'>
+        <img src='/logo.svg' alt='Post it logo' width={140} height={80} />
+      </Link>
       <div className='text-muted-foreground hidden lg:flex lg:flex-1 lg:gap-2 lg:items-center'>
         <SearchIcon />
         <Search className='w-full max-w-2xl' />
