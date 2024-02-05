5482cbb9b64e716f5991833d2f0b2f32415ef049
Ivan Atias <ivan.d.atias@gmail.com>
2024-02-05 02:48:04 -0400

    🔧 refactor: fix search box styles

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -6,22 +6,28 @@ import { PlusIcon, SearchIcon } from 'lucide-react'
 
 export function Navbar() {
   return (
-    <nav className='relative flex items-center justify-between w-full gap-3 py-3 lg:py-6'>
-      <Link href='/'>
-        <img src='/logo.svg' alt='Post it logo' width={140} height={80} />
-      </Link>
-      <div className='text-muted-foreground hidden lg:flex lg:flex-1 lg:gap-2 lg:items-center'>
-        <SearchIcon />
-        <Search className='w-full max-w-2xl' />
-      </div>
-      <div className='flex items-center gap-4'>
-        <UserAuthButton />
-        <MobileWidgets />
-        <Link href='/create-post' title='Create post' className='text-red-500'>
-          <PlusIcon className='hover:drop-shadow-[0_0_2px_#ff0000] w-6 h-6' />
-          <span className='sr-only'>Create post</span>
+    <header className='px-4 lg:px-8'>
+      <nav className='flex items-center justify-between w-full gap-3 py-3 lg:py-6'>
+        <Link href='/'>
+          <img src='/logo.svg' alt='Post it logo' width={140} height={80} />
         </Link>
-      </div>
-    </nav>
+        <div className='text-muted-foreground hidden lg:flex lg:flex-1 lg:gap-2 lg:items-center'>
+          <SearchIcon />
+          <Search className='w-full max-w-2xl' />
+        </div>
+        <div className='flex items-center gap-4'>
+          <UserAuthButton />
+          <MobileWidgets />
+          <Link
+            href='/create-post'
+            title='Create post'
+            className='text-red-500'
+          >
+            <PlusIcon className='hover:drop-shadow-[0_0_2px_#ff0000] w-6 h-6' />
+            <span className='sr-only'>Create post</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
   )
 }
